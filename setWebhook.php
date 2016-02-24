351c2fb66855a831c4110f071615fc7cb19ddeb9
Wasin Thonkaew <haxpor@gmail.com>
2016-02-25 00:19:43 +0700

    Add PRODUCTION setting to using ngrok or real production server for Telegram bot
    Set PRODUCTION to 0 will use ngork URL for development.
    Set PRODUCTION to 1 will use real production server.

--- setWebhook.php
+++ setWebhook.php
@@ -1,15 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-define('WEBHOOK_URL', 'https://wasin.io/projs/wasin-telegram-bot/api.php');
-
 if (php_sapi_name() == 'cli') {
 	
-  include('functions.php');
-  $configs = include 'config.php';
+	include('functions.php');
+	$configs = include 'config.php';
 
-  // if run from console, set or delete webhook
-  apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
-  exit;
+	if (PRODUCTION)
+	{
+		define('WEBHOOK_URL', 'https://wasin.io/projs/wasin-telegram-bot/api.php');
+	}
+	else
+	{
+		define('WEBHOOK_URL', 'https://325d46ae.ngrok.io/~haxpor/wasin-telegram-bot/api.php');
+	}
+
+	// if run from console, set or delete webhook
+	apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
+	exit;
 }
 ?>
